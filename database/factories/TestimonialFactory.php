88be6b445ca90373418ebaeb258006e4bc2ddbdd
ghassan <ghassan@digitalorder.net>
2024-12-29 14:43:38 +0200

    test

--- database/factories/TestimonialFactory.php
+++ database/factories/TestimonialFactory.php
@@ -21,19 +21,19 @@ class TestimonialFactory extends Factory
 
         return [
 
-            'opinion_ar'=> $faker->sentence(30),
-            'opinion_en'=>$faker->sentence(30),
-            'name_ar'=> $faker->sentence(12),
-            'name_en'=> $faker->sentence(12),
-            'age'=>$faker->numberBetween(20, 60),
-            'country'=>$faker->country(),
-            'gender'=>'male',
-            'job_title_ar'=>$faker->sentence(2),
-            'job_title_en'=>$faker->sentence(2),
-            'rating'=>$faker->numberBetween(1, 5),
-            'status' => 'on',
-            'photo' => '',
-            'language' => 'ar_en',
+            // 'opinion_ar'=> $faker->sentence(30),
+            // 'opinion_en'=>$faker->sentence(30),
+            // 'name_ar'=> $faker->sentence(12),
+            // 'name_en'=> $faker->sentence(12),
+            // 'age'=>$faker->numberBetween(20, 60),
+            // 'country'=>$faker->country(),
+            // 'gender'=>'male',
+            // 'job_title_ar'=>$faker->sentence(2),
+            // 'job_title_en'=>$faker->sentence(2),
+            // 'rating'=>$faker->numberBetween(1, 5),
+            // 'status' => 'on',
+            // 'photo' => '',
+            // 'language' => 'ar_en',
         ];
 
 
