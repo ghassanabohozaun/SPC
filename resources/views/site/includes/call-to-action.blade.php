ddc1e49c1ba4631185294afeb1cc7e2b8a6738e7
ghassan <ghassan@digitalorder.net>
2025-01-10 13:22:09 +0200

    test

--- resources/views/site/includes/call-to-action.blade.php
+++ resources/views/site/includes/call-to-action.blade.php
@@ -7,12 +7,12 @@
                     <img src="{!! asset('site/assets/images/help/icons/headphone.png') !!}" alt="Call Icon">
                 </div>
                 <div class="left-side-text">
-                    <h4>{!! trans('site.if_you_have_any_questions_schedule_an_appointment') !!}</h4>
-                    <h2>{!! trans('site.or_call_us_on') !!} <span>{!! setting()->site_mobile !!}</span></h2>
+                    <h4>{!! __('site.if_you_have_any_questions_schedule_an_appointment') !!}</h4>
+                    <h2>{!! __('site.or_call_us_on') !!} <span>{!! setting()->site_mobile !!}</span></h2>
                 </div>
             </div><!-- Left Side /-->
             <div class="right-side">
-                <a href="{!! route('appointment') !!}" class="button secondary">{!! trans('site.make_an_appointment') !!}</a>
+                <a href="{!! route('appointment') !!}" class="button secondary">{!! __('site.make_an_appointment') !!}</a>
             </div><!-- right Side /-->
         </div><!-- cell /-->
 
