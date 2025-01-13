358cea855e2e91c897e83d7987848a3c49e9b4e6
ghassan <ghassan@digitalorder.net>
2025-01-13 10:27:32 +0200

    about_doctor

--- resources/views/site/includes/about_doctor_Tawial.blade.php
+++ resources/views/site/includes/about_doctor_Tawial.blade.php
@@ -1,13 +1,15 @@
-<div class="book-preview grey-bg dark-bg">
-    <img src="{!! asset('site/assets/images/doctor55.png') !!}" alt="">
-    <div class="grid-container grid-x grid-padding-x">
-        <div class="large-7 medium-7 small-12 large-offset-5 cell">
-            <div class="book-text">
-                <h2>{!! __('site.about') !!} <span>{!! __('site.doctor') !!}</span></h2>
-                <p class="my_lead">{!! strip_tags(aboutSite()->{'about_doctor_' . Lang()}) !!}</p>
+@if (isEmpty(aboutSite()->{'about_doctor_' . Lang()}))
+    <div class="book-preview grey-bg dark-bg">
+        <img src="{!! asset('site/assets/images/doctor55.png') !!}" alt="">
+        <div class="grid-container grid-x grid-padding-x">
+            <div class="large-7 medium-7 small-12 large-offset-5 cell">
+                <div class="book-text">
+                    <h2>{!! __('site.about') !!} <span>{!! __('site.doctor') !!}</span></h2>
+                    <p class="my_lead">{!! strip_tags(aboutSite()->{'about_doctor_' . Lang()}) !!}</p>
+                </div>
+                <!-- Book Text /-->
             </div>
-            <!-- Book Text /-->
-        </div>
-        <!-- Cell /-->
-    </div><!-- Grid Container /-->
-</div>
+            <!-- Cell /-->
+        </div><!-- Grid Container /-->
+    </div>
+@endif
